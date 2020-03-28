febf82f4e6ff2a7b926fa36beea15fa4fba25751
William Oldham <wegg7250@gmail.com>
2020-03-28 12:57:36 +0000

    Add Card + Content

--- src/pages/category.js
+++ src/pages/category.js
@@ -4,11 +4,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from 'gatsby'
 import ToggleButton from "../components/toggle_button"
-import { Row, Col, Button } from "react-bootstrap"
+import { Row, Col, Button, Card, Nav } from "react-bootstrap"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignLanguage } from '@fortawesome/free-solid-svg-icons'
 
 const CategoryButton = ({ category }) => (
-        <ToggleButton value={category}>{category}</ToggleButton>
-    )
+    <ToggleButton variant="info" value={category} className="flex-grow-1">{category}</ToggleButton>
+)
 
 const CategoryPage = ({ data }) => {
 
@@ -16,7 +19,33 @@ const CategoryPage = ({ data }) => {
         <>
             <SEO title="Practice by Category" />
             <Layout>
-                    {data.allCategory.nodes.map(category => <CategoryButton key={category.id} category={category.category} />)}
+
+                <Row className="justify-content-center">
+                    <Col lg={6}>
+                        <Card>
+                            <Card.Header>
+                                <Nav className="flex-column flex-sm-row px-2">
+                                    <Nav.Item className="mr-auto align-self-center">
+                                        <h6 className="my-2">Choose the categories to practice</h6>
+                                    </Nav.Item>
+                                    <Nav.Item className="text-right">
+                                        <Button>Practice! <FontAwesomeIcon icon={faSignLanguage}/></Button>
+                                    </Nav.Item>
+                                </Nav>
+                            </Card.Header>
+                            <Card.Body>
+
+                                <Card.Title></Card.Title>
+                                {data.allCategory.nodes.map(category => {
+                                return (<div className="d-flex flex-sm-row my-2">
+                                    <CategoryButton key={category.id} category={category.name} />
+                                    <Button className="ml-1"><FontAwesomeIcon icon={faSignLanguage}/></Button>
+                                </div>)
+                                })}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </Layout>
         </>
     )
@@ -26,13 +55,12 @@ export const query = graphql`
     query Categories {
         allCategory {
             nodes {
-            signs {
-                hint
-                sign
-                video_url
-            }
-            category
-            id
+                name
+                signs {
+                    hint
+                    sign
+                    video_url
+                }
             }
         }
     }
