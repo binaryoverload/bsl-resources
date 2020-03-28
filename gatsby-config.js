ace6a404c027c39cef336e4671e563b8c869ad8f
William Oldham <wegg7250@gmail.com>
2020-03-28 14:00:18 +0000

    Proper Author

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `BSL Resources`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@binaryoverload`,
+    author: `William Oldham`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
