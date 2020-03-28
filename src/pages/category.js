0479c8a847b643cddf8b3afdbedbdc0288203f3b
William Oldham <wegg7250@gmail.com>
2020-03-28 14:01:31 +0000

    Improve category ui

--- src/pages/category.js
+++ src/pages/category.js
@@ -1,35 +1,33 @@
 import React from "react"
 
+import { graphql } from 'gatsby'
+
+import { titleCase } from "../utils/title-case"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { graphql } from 'gatsby'
-import ToggleButton from "../components/toggle_button"
+import ToggleButton from "../components/toggle-button"
 import { Row, Col, Button, Card, Nav } from "react-bootstrap"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignLanguage } from '@fortawesome/free-solid-svg-icons'
 
-const CategoryButton = ({ category }) => (
-    <ToggleButton variant="info" value={category} className="flex-grow-1">{category}</ToggleButton>
-)
-
 const CategoryPage = ({ data }) => {
-
     return (
         <>
             <SEO title="Practice by Category" />
             <Layout>
-
                 <Row className="justify-content-center">
                     <Col lg={6}>
                         <Card>
                             <Card.Header>
                                 <Nav className="flex-column flex-sm-row px-2">
-                                    <Nav.Item className="mr-auto align-self-center">
+                                    <Nav.Item className="mr-auto align-self-center w-75">
                                         <h6 className="my-2">Choose the categories to practice</h6>
+                                        <p className="text-muted">Select multiple categories to practice or choose just one by clicking <FontAwesomeIcon icon={faSignLanguage}/>!</p>
                                     </Nav.Item>
-                                    <Nav.Item className="text-right">
-                                        <Button>Practice! <FontAwesomeIcon icon={faSignLanguage}/></Button>
+                                    <Nav.Item className="text-right align-self-center">
+                                        <Button>Practice!</Button>
                                     </Nav.Item>
                                 </Nav>
                             </Card.Header>
@@ -38,7 +36,13 @@ const CategoryPage = ({ data }) => {
                                 <Card.Title></Card.Title>
                                 {data.allCategory.nodes.map(category => {
                                 return (<div className="d-flex flex-sm-row my-2">
-                                    <CategoryButton key={category.id} category={category.name} />
+                                    <ToggleButton 
+                                        variant="info" 
+                                        value={category.FontAwesomeIcon} 
+                                        className="flex-grow-1"
+                                        key={category}>
+                                            {titleCase(category.name)}
+                                    </ToggleButton>
                                     <Button className="ml-1"><FontAwesomeIcon icon={faSignLanguage}/></Button>
                                 </div>)
                                 })}
