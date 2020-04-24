b3f98606bc1b4800d67d44127fc9f2fafcae53db
William Oldham <wegg7250@gmail.com>
2020-04-24 22:35:58 +0100

    Revert "Make the image component more dynamic"
    This reverts commit 5b85f828f8aeafb317017556d9cd28bb6bb7f77d.

--- src/components/image.js
+++ src/components/image.js
@@ -13,12 +13,12 @@ import Img from "gatsby-image"
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const Image = ({ src, maxWidth }) => {
+const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "${src}" }) {
+      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
         childImageSharp {
-          fluid(maxWidth: ${maxWidth}) {
+          fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
           }
         }
