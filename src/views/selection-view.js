3cf87e223a19e9d91baaafe7ee55174fde9a20f4
William Oldham <wegg7250@gmail.com>
2020-04-26 21:30:27 +0100

    Sort by display name and fix method call

--- src/views/selection-view.js
+++ src/views/selection-view.js
@@ -12,32 +12,36 @@ import { Selection, SelectionButton } from "../components/Selection"
 const SelectionView = ({ grouping, onChange }) => {
     const [selectedGroups, setSelectedGroups] = useState([])
 
-    const sortByName = (a, b) => {
-        if (a.name > a.name) return 1;
-        if (a.name < b.name) return -1;
+    const sortByDisplayName = (a, b) => {
+        if (a.display_name > a.display_name) return 1;
+        if (a.display_name < b.display_name) return -1;
 
         return 0;
     }
 
+    let practiceButtonText = selectedGroups.length === 0 ?
+                                grouping.meta.all :
+                                `${grouping.results.filter(result => selectedGroups.includes(result.name)).flatMap(result => result.elements).length} Signs`
+
     return (
-        <Card>
+        <Card className="my-3">
             <Card.Header>
                 <Nav className="flex-column flex-sm-row px-2">
-                    <Nav.Item className="mr-auto align-self-center w-75">
-                        <h6 className="my-2">Choose the {grouping.meta.display_name.toLower()} to practice</h6>
-                        <p className="text-muted">Select multiple {grouping.meta.display_name.toLower()} to practice or choose just one by clicking <FontAwesomeIcon icon={faSignLanguage} />!</p>
+                    <Nav.Item className="mr-auto align-self-center w-50">
+                        <h6 className="my-2">Choose the {grouping.meta.display_name.toLowerCase()} to practice</h6>
+                        <p className="text-muted">Select multiple {grouping.meta.display_name.toLowerCase()} to practice or choose just one by clicking <FontAwesomeIcon icon={faSignLanguage} />!</p>
                     </Nav.Item>
-                    <Nav.Item className="text-right align-sef-center">
+                    <Nav.Item className="text-right align-self-center">
                         <Link to={`/practice${selectedGroups.length > 0 ? `?${grouping.meta.name}=${selectedGroups.join(",")}` : ''}`}>
-                            <Button>Practice!</Button> {/* TODO:  Dynamic button? */}
+                            <Button>Practice {practiceButtonText}!</Button>
                         </Link>
                     </Nav.Item>
                 </Nav>
             </Card.Header>
             <Card.Body>
                 <Selection onChange={setSelectedGroups} groupingKey={grouping.meta.name}>
-                    {grouping.results.sort(sortByName).map(group => (
-                        <div key={group.id} className="d-flex flex-sm-row my-2">
+                    {grouping.results.sort(sortByDisplayName).map(group => (
+                        <div key={group.name} className="d-flex flex-sm-row my-2">
                             <SelectionButton
                                 variant="info"
                                 name={group.name}
