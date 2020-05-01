a88750efe737635e66166ccca1b4c8fdf9f0d042
William Oldham <wegg7250@gmail.com>
2020-05-01 21:14:31 +0100

    Make everything look better!

--- src/views/selection-view.js
+++ src/views/selection-view.js
@@ -1,7 +1,7 @@
 
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
-import { Button, Card, Nav } from "react-bootstrap"
+import { Button, Card, Nav, Form } from "react-bootstrap"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSignLanguage } from '@fortawesome/free-solid-svg-icons'
@@ -11,45 +11,52 @@ import { Selection, SelectionButton } from "../components/Selection"
 
 const SelectionView = ({ grouping, onChange }) => {
     const [selectedGroups, setSelectedGroups] = useState([])
+    const [filter, setFilter] = useState("")
+    const [data, setData] = useState(grouping.data.sort(sortByDisplayName))
+
+    useEffect(() => {
+        setData(grouping.data.sort(sortByDisplayName).filter((grouping) => !filter || grouping.display_name.toLowerCase().includes(filter.toLowerCase())))
+    }, [filter, grouping.data])
 
     const sortByDisplayName = (a, b) => {
-        if (a.display_name > a.display_name) return 1;
+        if (a.display_name > b.display_name) return 1;
         if (a.display_name < b.display_name) return -1;
 
         return 0;
     }
 
     let practiceButtonText = selectedGroups.length === 0 ?
-                                grouping.meta.all :
-                                `${grouping.results.filter(result => selectedGroups.includes(result.name)).flatMap(result => result.elements).length} Signs`
+                                grouping.all :
+                                `${grouping.data.filter(result => selectedGroups.includes(result.name)).flatMap(result => result.signs).length} Signs`
 
     return (
         <Card className="my-3">
-            <Card.Header>
-                <Nav className="flex-column flex-sm-row px-2">
-                    <Nav.Item className="mr-auto align-self-center w-50">
-                        <h6 className="my-2">Choose the {grouping.meta.display_name.toLowerCase()} to practice</h6>
-                        <p className="text-muted">Select multiple {grouping.meta.display_name.toLowerCase()} to practice or choose just one by clicking <FontAwesomeIcon icon={faSignLanguage} />!</p>
+            <Card.Header className="sticky-top pb-3" style={{"background-color": "#FFF"}}>
+                <Nav className="d-flex flex-md-row flex-column mb-3 flex-nowrap">
+                    <Nav.Item id="selection-header" className="mr-auto">
+                        <h6 className="my-2">Choose the {grouping.display_name.toLowerCase()} to practice</h6>
+                        <p className="text-muted m-0">Select multiple {grouping.display_name.toLowerCase()} to practice or choose just one by clicking <FontAwesomeIcon icon={faSignLanguage} />!</p>
                     </Nav.Item>
-                    <Nav.Item className="text-right align-self-center">
-                        <Link to={`/practice${selectedGroups.length > 0 ? `?${grouping.meta.name}=${selectedGroups.join(",")}` : ''}`}>
+                    <Nav.Item id="selection-button" className="align-self-center flex-shrink-0">
+                        <Link to={`/practice${selectedGroups.length > 0 ? `?${grouping.name}=${selectedGroups.join(",")}` : ''}`}>
                             <Button>Practice {practiceButtonText}!</Button>
                         </Link>
                     </Nav.Item>
                 </Nav>
+                <Form.Control onChange={(value) => setFilter(value.target.value)} placeholder="Filter" type="search"></Form.Control>
             </Card.Header>
             <Card.Body>
-                <Selection onChange={setSelectedGroups} groupingKey={grouping.meta.name}>
-                    {grouping.results.sort(sortByDisplayName).map(group => (
-                        <div key={group.name} className="d-flex flex-sm-row my-2">
+                <Selection onChange={setSelectedGroups} groupingKey={grouping.name}>
+                    {data.map(group => (
+                        <div key={group.name} className="d-flex flex-row my-2">
                             <SelectionButton
                                 variant="outline-secondary"
                                 name={group.name}
-                                groupingKey={grouping.meta.name}
+                                groupingKey={grouping.name}
                                 className="flex-grow-1">
                                 {group.display_name}
                             </SelectionButton>
-                            <Link to={`/practice?${grouping.meta.name}=${group.name}`}>
+                            <Link to={`/practice?${grouping.name}=${group.name}`}>
                                 <Button className="ml-1"><span className="sr-only">Practice {group.name}</span><FontAwesomeIcon icon={faSignLanguage} /></Button>
                             </Link>
                         </div>
