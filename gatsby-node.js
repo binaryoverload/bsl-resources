435c4d898b9424f68c501860612c067701f33d78
William Oldham <wegg7250@gmail.com>
2020-05-01 21:10:55 +0100

    Data processing!

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,43 +27,52 @@ function makeSignNode(sign, { createNodeId, createContentDigest }) {
 
 exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
     const fileBase = "./src/data/"
-    const signData = await getData(fileBase + "signs.csv")
+    const signData = await getData(fileBase + "signs.tsv")
 
     const groupingData = {}
-    
-    for (let file of fs.readdirSync(fileBase + "groupings/")) {
-        if (!file.endsWith(".csv")) continue
-        const fileData = await getData(fileBase + "groupings/" + file)
-        if (fileData.meta) {
-            groupingData[fileData.meta.name || file.substring(0, file.lastIndexOf("."))] = fileData
-        } else {
-            groupingData[file.substring(0, file.lastIndexOf("."))] = fileData
+
+    const groupingMeta = await getData(fileBase + "groupings/groupings_meta.tsv")
+    if (groupingData.error) throw groupingData.error
+
+    for (let grouping of groupingMeta.results) {
+        const name = grouping.name
+        grouping.data = []
+
+        // name, sign
+        const groupDataIn = await getData(fileBase + "groupings/" + name + ".tsv")
+        if (groupDataIn.error) throw groupDataIn.error
+
+        // name, display_name
+        const groupMetaIn = await getData(fileBase + "groupings/" + name + "_meta.tsv")
+        if (groupDataIn.error) throw groupDataIn.error
+
+        for (let groupData of groupDataIn.results) {
+            const data = grouping.data.find((data) => data.name === groupData.name)
+            if (!data) {
+                grouping.data.push({
+                    name: groupData.name,
+                    signs: [groupData.sign]
+                })
+            } else {
+                data.signs = [...(data.signs || []), groupData.sign]
+            }
         }
-    }
 
-    signData.results.forEach(sign => {
-        for (grouping in groupingData) {
-            groupingData[grouping].results.forEach(row => {
-                if (row.elements.includes(sign.sign)) {
-                    sign[grouping] = [...(sign[grouping] || []), row.name]
-                }
-            })
-
-            if (!sign[grouping]) {
-                sign[grouping] = [groupingData[grouping].meta.unknown]
-                let unknownGroup = groupingData[grouping].results.find(result => result.name === groupingData[grouping].meta.unknown);
-                if (!unknownGroup) {
-                    groupingData[grouping].results.push({
-                        display_name: groupingData[grouping].meta.unknown_display,
-                        name: groupingData[grouping].meta.unknown,
-                        elements: [sign.sign]
-                    })
-                } else {
-                    unknownGroup.elements.push(sign.sign)
+        for (let groupMeta of groupMetaIn.results) {
+            const data = grouping.data.find((data) => data.name === groupMeta.name)
+            if (!data) {
+                grouping.data.push({ ...groupMeta })
+            } else {
+                for (let key in groupMeta) {
+                    data[key] = groupMeta[key]
                 }
             }
         }
 
+        groupingData[name] = grouping
+    }
+
+    signData.results.forEach(sign => {
         const node = makeSignNode(sign, { createNodeId, createContentDigest })
         actions.createNode(node);
     });
@@ -81,54 +90,31 @@ exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => 
         };
 
         actions.createNode(node);
-        
+
     })
 }
 
-function getFileMeta(fileName) {
-    const regex = /^---(?:\r?\n)?([^]+)(?:\r?\n)?---/;
-    try {
-        let file = fs.readFileSync(fileName).toString()
-        let match;
-        let meta;
-        if ((match = regex.exec(file)) !== null) {
-            file = file.toString().substring(match[0].length).trim()
-            let metaLines = match[1].split("\n").filter(s => s !== "")
-            meta = {}
-            for (const line of metaLines) {
-                const kv = line.split(":")
-                if (kv.length === 2) {
-                    meta[kv[0].trim()] = kv[1].trim()
-                }
-            }
-        }
-        return { file, meta }
-    } catch (e) {
-        console.error(e)
-        return { error: e }
-    }
-}
 
 function getData(fileName) {
-    const { file, meta, error } = getFileMeta(fileName);
-
     return new Promise(resolve => {
-        if (error) resolve({ error })
+        let results = []
 
-        let result = { meta, results: [] }
-
+        const file = fs.readFileSync(fileName).toString()
         Readable.from(file.split("\n"))
             .pipe(csv({
-                mapValues: ({header, value}) => {
+                mapHeaders: ({ header }) => header === "" ? null : header,
+                mapValues: ({ header, value }) => {
                     if (header === "elements") {
                         return value.split(";")
                     }
                     return value
-                }
+                },
+                separator: "\t"
             }))
-            .on('data', (data) => result.results.push(data))
+            .on('data', (data) => results.push(data))
+            .on('error', (err) => resolve({ err }))
             .on('end', () => {
-                resolve(result)
+                resolve({ results })
             });
     })
 }
