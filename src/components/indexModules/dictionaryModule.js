9fd0784e7fd959cf6ad9e8e409ccb440e68bc401
William Oldham <wegg7250@gmail.com>
2020-05-02 12:49:36 +0100

    Make content on main page more uniform

--- src/components/indexModules/dictionaryModule.js
+++ src/components/indexModules/dictionaryModule.js
@@ -1,14 +1,18 @@
 import React from "react"
 
+import { Link } from "gatsby"
 import { Card, Button } from "react-bootstrap"
 
 const DictionaryModule = () => (
     <Card className="my-1">
-        <Card.Body>
+        <Card.Body style={{minHeight: "15rem"}}>
             <Card.Title>Sign dictionaries</Card.Title>
             <p>Here you can find some sign dictionaries to help you find a more obscure word!</p>
-            <Button href="https://www.signbsl.com/"><img className="align-self-center" alt="Sign BSL Logo" src="https://www.signbsl.com/logo.svg" style={{ height: "1.5em" }} /> SignBSL</Button>{' '}
-            <Button href="https://spreadthesign.com"><img alt="Spread The Sign Logo" src="https://www.spreadthesign.com/static/img/favicon.ico" style={{ height: "1em" }} /> SpreadTheSign</Button>
+            <p>If we don't have a word in our database, please consider <Link to="/contact">contacting us</Link> and we will be sure to add it!</p>
+            <div className="d-flex justify-content-center mt-auto">
+                <Button className="mr-2" href="https://www.signbsl.com/"><img className="align-self-center" alt="Sign BSL Logo" src="https://www.signbsl.com/logo.svg" style={{ height: "1.5em" }} /> SignBSL</Button>{' '}
+                <Button href="https://spreadthesign.com"><img alt="Spread The Sign Logo" src="https://www.spreadthesign.com/static/img/favicon.ico" style={{ height: "1em" }} /> SpreadTheSign</Button>
+            </div>
         </Card.Body>
     </Card>
 )
--- src/components/indexModules/practiceModule.js
+++ src/components/indexModules/practiceModule.js
@@ -4,17 +4,16 @@ import { Card, Button } from "react-bootstrap"
 
 const PracticeModule = () => (
     <Card className="my-1">
-        <Card.Body>
+        <Card.Body style={{minHeight: "15rem"}}>
             <Card.Title>Sign practice</Card.Title>
             <Card.Text>
                 Practice sign using the words from the sessions. Each sign comes with a hint to help you remember it as well as a video.
                 <br /><br />
                 Signs are grouped per category and per-week to help you practice specific topics or signs from a specific session.
               </Card.Text>
-            <div className="d-flex justify-content-around">
-                <Button className="mr-1" href="/category">Practice by category</Button>{' '}
-                <Button className="mx-1" href="/week">Practice by Week</Button>{' '}
-                <Button className="ml-1" variant="dark" href="/practice">Practice Everything</Button>
+            <div className="d-flex justify-content-center mt-auto">
+                <Button className="mr-2" href="/selection">Select signs to practice</Button>
+                <Button variant="dark" href="/practice">Practice Everything</Button>
             </div>
         </Card.Body>
     </Card>
