0ec74d609c09a229427d0a7e55ce55751e6ed6af
William Oldham <wegg7250@gmail.com>
2020-05-02 12:49:59 +0100

    Add a contact page!

---
+++ src/pages/contact.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+const ContactPage = () => (
+    <>
+      <SEO title="Contact Us" />
+      <Layout>
+
+      </Layout>
+    </>
+  )
+  
+  export default ContactPage
