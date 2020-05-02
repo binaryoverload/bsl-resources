9f43e2fadcbf41694f25858c347b51fb3d8e585a
William Oldham <wegg7250@gmail.com>
2020-05-02 11:18:56 +0100

    Add alt text to images

--- src/components/indexModules/dictionaryModule.js
+++ src/components/indexModules/dictionaryModule.js
@@ -7,8 +7,8 @@ const DictionaryModule = () => (
         <Card.Body>
             <Card.Title>Sign dictionaries</Card.Title>
             <p>Here you can find some sign dictionaries to help you find a more obscure word!</p>
-            <Button href="https://www.signbsl.com/"><img className="align-self-center" src="https://www.signbsl.com/logo.svg" style={{ height: "1.5em" }} /> SignBSL</Button>{' '}
-            <Button href="https://spreadthesign.com"><img src="https://www.spreadthesign.com/static/img/favicon.ico" style={{ height: "1em" }} /> SpreadTheSign</Button>
+            <Button href="https://www.signbsl.com/"><img className="align-self-center" alt="Sign BSL Logo" src="https://www.signbsl.com/logo.svg" style={{ height: "1.5em" }} /> SignBSL</Button>{' '}
+            <Button href="https://spreadthesign.com"><img alt="Spread The Sign Logo" src="https://www.spreadthesign.com/static/img/favicon.ico" style={{ height: "1em" }} /> SpreadTheSign</Button>
         </Card.Body>
     </Card>
 )
