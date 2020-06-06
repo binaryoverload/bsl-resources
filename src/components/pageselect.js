bcd68dad8105f3e2dd4888f22c467304cf1a7771
William Oldham <wegg7250@gmail.com>
2020-06-06 15:57:41 +0100

    Carosel

---
+++ src/components/pageselect.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle, faDotCircle } from '@fortawesome/free-regular-svg-icons'
+
+const PageSelect = ({current, size, callback}) => {
+
+    const dots = []
+
+    for (let i = 0; i < size; i++) {
+        if (i == current) {
+            dots.push(
+                <a href="#" style={{ margin: "0 0.1em" }} key={i} onClick={() => callback(i)}><FontAwesomeIcon icon={faDotCircle}/></a>
+            )
+        } else {
+            dots.push(
+                <a href="#" style={{ margin: "0 0.1em" }} key={i} onClick={() => callback(i)}><FontAwesomeIcon icon={faCircle}/></a>
+            ) 
+        }
+    }
+
+    return <div style={{ margin: "0.5em 0"}}>
+        {dots}
+    </div>
+}
+
+export default PageSelect
