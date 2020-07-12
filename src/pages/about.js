04c7b6cdc8a13db87b6104077439b69e161f466d
William Oldham <wegg7250@gmail.com>
2020-07-12 19:12:15 +0100

    Non-empty about page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+import EmailLink from "../components/EmailLink"
+import { Form, Button, Row, Col } from "react-bootstrap"
+
+const AboutPage = () => (
+    <>
+        <SEO title="About The Society" />
+        <Layout className="pt-5">
+            
+        </Layout>
+    </>
+)
+
+export default AboutPage
